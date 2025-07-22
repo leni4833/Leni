4041c608f3aba4d561c68260f632a3f513de641c
Leni <leni.802430@sxcce.edu.in>
2025-07-22 15:51:35 +0530

    build fix

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function  About(){
-    return(
-    <div className="flex justify-center items-center h-screen">
-        <h2>Who am I?</h2><br></br>
-        <h3>I'm Emma Smith, a visual UX/UI Designer and Web Developer</h3><br></br>
-        <p>I am a freelancer based in the United Kingdom and i have been building noteworthy UX/UI designs and websites for years, which comply with the latest design trends. I help convert a vision and an idea into meaningful and useful products. Having a sharp eye for product evolution helps me prioritize tasks, iterate fast and deliver faster.</p>
-    </div>
-    )
-}
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,19 +5,18 @@ export default function Resume(){
         <h3 className="text-2xl font-semibold mb-4 capitalize">Education</h3>
         <h2 className="font-bold">Computer Science</h2>
         <p>Cambridge University / 2004 - 2007
-           Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Optio quo repudiandae.</p>
+          “Be yourself; everyone else is already taken.”</p>
         <div><br></br>
             <h2 className="font-bold">Bachelor Degree</h2>
             <p>University of Tokyo / 2008 - 2010
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.</p>
+“Be yourself; everyone else is already taken.”</p>
         </div><br></br>
         <div>
             <h2 className="font-bold">Master Degree</h2>
             <p>Harvard University / 2012 - 2015
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.</p>
+“Be yourself; everyone else is already taken.”</p>
         </div>
         </div>
         <div>{/* Right side */}
@@ -26,19 +25,19 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.
             <h2 className="font-bold">Software Engineer</h2>
             <p>Adobe / 2015 - 2017
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.</p>
+“Be yourself; everyone else is already taken.”</p>
         </div><br></br>
         <div>
             <h2 className="font-bold">Back-End Developer</h2>
             <p>Google / 2017 - 2018
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.</p>
+“Be yourself; everyone else is already taken.”</p>
         </div><br></br>
         <div>
             <h2 className="font-bold">UI/UX Designer</h2>
             <p>Discord / 2019 - Present
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quo repudiandae.</p>
+“Be yourself; everyone else is already taken.”</p>
         </div>
         </div>
         </div>
