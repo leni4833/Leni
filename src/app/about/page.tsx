724180581e62816f7fb242b7188ec19cbe58b1e0
Leni <leni.802430@sxcce.edu.in>
2025-07-22 15:43:46 +0530

    build fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,13 @@ export default function  About(){
     return(
     <div className="flex flex-col justify-center items-center h-screen px-6 text-center max-w-3xl mx-auto">
       <h3 className="text-2xl font-semibold mb-4 capitalize">Who am I?</h3>
-      <p className=" text-gray-700 ">
-        I am a freelancer based in the United Kingdom, and I have been building
-        noteworthy UX/UI designs and websites for years, which comply with the latest
-        design trends. I help convert a vision and an idea into meaningful and useful
-        products. Having a sharp eye for product evolution helps me prioritize tasks,
-        iterate fast, and deliver faster.
-      </p>
+      <p className="text-gray-700">
+  I&apos;m a freelancer based in the United Kingdom, and I have been building
+  noteworthy UX/UI designs and websites for years, which comply with the latest
+  design trends. I help convert a vision and an idea into meaningful and useful
+  products. Having a sharp eye for product evolution helps me prioritize tasks,
+  iterate fast, and deliver faster.
+</p>
     </div>
     )
 }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,21 +1,15 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div
-      className="flex flex-col justify-center items-center h-screen bg-cover bg-center"
-      style={{
-        backgroundImage:
-          "url('https://www.barberstake.com/wp-content/uploads/2024/10/Low-Taper-Fade-Haircut-1-995x503.jpg')",
-      }}
-    >
-      <h1 className="text-6xl md:text-8xl font-bold text-gray-300 drop-shadow-lg">
-        Emma Smith
-      </h1>
-      <h3 className="text-2xl md:text-3xl font-semibold mt-4 text-gray-300 drop-shadow-md">
-        Designer
-      </h3>
-    </div>
+    <div className="flex flex-col justify-center items-center h-screen bg-cover bg-center"
+      
+      style={{ backgroundImage: "url('https://www.barberstake.com/wp-content/uploads/2024/10/Low-Taper-Fade-Haircut-1-995x503.jpg')", }}
+      >
+        <h1 className="text-8xl font-bold text-gray-300">Emma Smith</h1>
+        <h3 className="text-3xl font-semibold mt-4 text-gray-300">Designer</h3>
+      </div>
+  
   );
 }
 
